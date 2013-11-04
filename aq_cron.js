39b765c4be566efde30cd7a180fd48db354201b6
Timothy <yexiaozhou2003@gmail.com>
2013-11-04 23:47:22 +0800

    implement cron job

---
+++ aq_cron.js
@@ -0,0 +1,12 @@
+var cronJob = require('cron').CronJob;
+var request = require('request');
+var fs = require('fs');
+
+var job = new cronJob('*/1 * * * *', function(){
+	request('http://www.pm25.in/api/querys/pm2_5.json?city=chengdu&avg=false&stations=yes&token=9zsQCyiC3cdFR8GzaCsp').pipe(
+		fs.createWriteStream('data.json'));
+  }, function () {
+    // This function is executed when the job stops
+  },
+  true /* Start the job right now */
+);

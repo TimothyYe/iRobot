51828d09b9e5d770bfb9a142caafbe8e06063751
Timothy <yexiaozhou2003@gmail.com>
2013-11-05 23:28:47 +0800

    add xml response

--- routes/message.js
+++ routes/message.js
@@ -3,7 +3,7 @@
  */
 var parseString = require('xml2js').parseString;
 var fs = require('fs');
-var content = '';
+var builder = require('xmlbuilder');
 
 function readDetailData (callback){
 
@@ -30,10 +30,33 @@ function readDetailData (callback){
 		});
 }
 
-exports.index = function(req, res){
-		console.log(req.rawBody.toString());
+function createXmlResponse(jsonObj, content, callback){
+	var doc = builder.create();
 
-		parseString(req.rawBody, function (err, result) {
+	doc.begin('xml')
+		.ele('ToUserName')
+			.txt(jsonObj.xml.FromUserName[0])
+		.up()
+		.ele('FromUserName')
+			.txt('PowerfulTim')
+		.up()
+		.ele('CreateTime')
+			.txt('12345678')
+		.up()
+		.ele('MsgType')
+			.txt('text')
+		.up()
+		.ele('Content')
+			.txt(content);
+
+	  var ret = doc.toString({ pretty: true});
+		callback(ret);
+}
+
+exports.index = function(req, res){
+	console.log(req.rawBody.toString());
+
+	parseString(req.rawBody, function (err, result) {
 		console.dir(result);
 
 		console.log(result.xml.FromUserName[0]);
@@ -42,12 +65,16 @@ exports.index = function(req, res){
 
 		if(result["xml"]["Content"][0].indexOf("空气") != -1 ||
 			result["xml"]["Content"][0].indexOf("质量") != -1) {
-				 readDetailData(function(content){
-				console.log(content);
-});
+				readDetailData(function(content){
+					console.log(content);
+
+					createXmlResponse(result, content, function(xmlString){
+						console.log(xmlString);
+						res.send(xmlString);
+					});
+				});
 			}
 
-		res.send('ok');
-		});
-	};
+	});
+};
 

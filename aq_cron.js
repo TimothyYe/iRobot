2b3b2366f657fa48150cb9f5db7708c93b938f6d
Timothy <yexiaozhou2003@gmail.com>
2013-11-05 22:28:13 +0800

    implement response functionality

--- aq_cron.js
+++ aq_cron.js
@@ -5,6 +5,9 @@ var fs = require('fs');
 var job = new cronJob('*/1 * * * *', function(){
 	request('http://www.pm25.in/api/querys/pm2_5.json?city=chengdu&avg=false&stations=yes&token=9zsQCyiC3cdFR8GzaCsp').pipe(
 		fs.createWriteStream('data.json', { encoding: 'utf-8'}));
+
+	request('http://www.pm25.in/api/querys/pm2_5.json?city=chengdu&avg=true&stations=no&token=9zsQCyiC3cdFR8GzaCsp').pipe(
+		fs.createWriteStream('avg.json', { encoding: 'utf-8'}));
   }, function () {
     // This function is executed when the job stops
   },
--- routes/message.js
+++ routes/message.js
@@ -2,6 +2,8 @@
  * POST message page.
  */
 var parseString = require('xml2js').parseString;
+var fs = require('fs');
+
 
 exports.index = function(req, res){
 		console.log(req.rawBody.toString());
@@ -15,9 +17,34 @@ exports.index = function(req, res){
 
 		if(result["xml"]["Content"][0].indexOf("空气") != -1 ||
 			result["xml"]["Content"][0].indexOf("质量") != -1) {
-				console.log('Yes!');
+				var content = readDetailData();
+				console.log(content);
 			}
 
 		res.send('ok');
 		});
 	};
+
+var readDetailData = function(){
+
+	var file = './data.json';
+	var obj;
+	var time;
+	var content;
+
+		fs.readFile(file, 'utf-8', function(err, data){
+			if(err) {
+				console.log('err!');
+				return;
+			}
+
+			obj = JSON.parse(data);
+			time = obj[0].time_point;
+			content += 'Latest update:' + time + '\n';
+
+			for(var aq in obj){
+				content += obj[aq].area + '-' + obj[aq].position_name + ' AQ:' + obj[aq].aqi + ' Level:' + obj[aq].quality + ' PM2.5:' + obj[aq].pm2_5;
+				content += '\n';
+			}
+		}
+	}
